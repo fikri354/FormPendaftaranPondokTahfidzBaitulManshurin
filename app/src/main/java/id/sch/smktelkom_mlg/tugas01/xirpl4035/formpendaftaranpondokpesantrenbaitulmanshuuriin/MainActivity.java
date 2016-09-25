4603b8851019c4a8b057b683f6f4fa449f523992
Zulfikri Rosyid <zulf354@gmail.com>
2016-09-25 16:32:44 +0700

    Inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4035.formpendaftaranpondokpesantrenbai
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -24,13 +25,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvNama = (TextView) findViewById(R.id.textViewNama);
-        tvRumah = (TextView) findViewById(R.id.textViewAlamat);
-        tvHafalan = (TextView) findViewById(R.id.textViewHafalan);
-        tvHafal = (TextView) findViewById(R.id.textViewHafal);
-        etNama = (EditText) findViewById(R.id.editTextNama);
-        etRumah = (EditText) findViewById(R.id.editTextRumah);
-        spHafalan = (Spinner) findViewById(R.id.spinnerHafalan);
-        bOk = (Button) findViewById(R.id.buttonOK);
+
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                {
+                    doclick();
+                }
+
+            }
+        });
+    }
+
+    private void doclick() {
+
     }
 }
