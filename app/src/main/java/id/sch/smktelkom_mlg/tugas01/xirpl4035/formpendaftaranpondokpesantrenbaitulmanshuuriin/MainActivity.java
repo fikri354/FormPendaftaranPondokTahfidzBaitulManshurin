d9410ca7cb718165a46bbbc9822e9c69d8ae00f9
Zulfikri Rosyid <zulf354@gmail.com>
2016-09-25 15:37:34 +0700

    Fungsi button

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
@@ -2,12 +2,35 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4035.formpendaftaranpondokpesantrenbai
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvNama;
+    TextView tvRumah;
+    TextView tvHafalan;
+    TextView tvHafal;
+    EditText etNama;
+    EditText etRumah;
+    Spinner spHafalan;
+    Button bOk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNama = (TextView) findViewById(R.id.textViewNama);
+        tvRumah = (TextView) findViewById(R.id.textViewAlamat);
+        tvHafalan = (TextView) findViewById(R.id.textViewHafalan);
+        tvHafal = (TextView) findViewById(R.id.textViewHafal);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etRumah = (EditText) findViewById(R.id.editTextRumah);
+        spHafalan = (Spinner) findViewById(R.id.spinnerHafalan);
+        bOk = (Button) findViewById(R.id.buttonOK);
     }
 }
