75ba946a7714cfc950c5a8ab4513a8640b4ac6a9
Zulfikri Rosyid <zulf354@gmail.com>
2016-10-01 22:40:57 +0700

    Inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4035/formpendaftaranpondokpesantrenbaitulmanshuuriin/MainActivity.java
@@ -3,10 +3,15 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4035.formpendaftaranpondokpesantrenbai
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
     TextView tvRumah;
     TextView tvHafalan;
     TextView tvHafal;
+    TextView tvHasil;
     EditText etNama;
     EditText etRumah;
     Spinner spHafalan;
+    String[] arHafalan = {"Juz 1", "Juz 2", "Juz 3"};
+    ArrayList<String> listHafalan = new ArrayList<>();
+    ArrayAdapter<String> adapter;
     Button bOk;
 
 
@@ -24,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listHafalan);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spHafalan.setAdapter(adapter);
+
+        spHafalan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                {
+                    listHafalan.clear();
+                    listHafalan.addAll(Arrays.asList(arHafalan[pos]));
+                    adapter.notifyDataSetChanged();
+                    spHafalan.setSelection(0);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         tvNama = (TextView) findViewById(R.id.textViewNama);
         tvRumah = (TextView) findViewById(R.id.textViewAlamat);
@@ -47,5 +77,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void doclick() {
 
+
     }
+
+
 }
